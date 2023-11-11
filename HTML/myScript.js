58ba472afab05e541e9cbc8ef133cfd25319629c
icemanmobil <59970080+icemanmobil@users.noreply.github.com>
2023-11-11 12:57:43 -0500

    new version

--- HTML/myScript.js
+++ HTML/myScript.js
@@ -1,11 +1,10 @@
-
 // document.addEventListener("DOMContentLoaded", function () {
 //     const submitBtn = document.getElementById("submitBtn");
 //     const errorDiv = document.getElementById("error-message");
 
 //     submitBtn.addEventListener("click", function () {
 //         const question1Value = document.getElementById("question1").value;
-        
+
 //         if (question1Value.trim() === "") {
 //             errorDiv.innerHTML = "Please answer the question before submitting.";
 //         } else {
@@ -15,38 +14,22 @@
 //     });
 // });
 
-
-
 document.addEventListener("DOMContentLoaded", function () {
-    const startTestButton = document.getElementById("startTestButton");
+  const startTestButton = document.getElementById("startTestButton");
+  debugger;
 
-    startTestButton.addEventListener("click", function () {
-        // Specify the URL of the first question page
-        const question1Url = "Questions/Q1.html";
+  startTestButton.addEventListener("click", function () {
+    // Specify the URL of the first question page
+    const question1Url = "Questions/Q1.html";
 
-        // Open the question page in a new window or tab
-        window.open(question1Url, "_blank");
-    });
+    // Open the question page in a new window or tab
+    window.open(question1Url, "_blank");
+  });
 });
 
-window.open("page2.html")
-
-window.close("index.html")
+window.close("index.html");
 
 function nextQuestion(nextPage) {
-    window.location.href = nextPage;
-    return false; // Prevent the form from submitting (to avoid page reload)
+  window.location.href = nextPage;
+  return false; // Prevent the form from submitting (to avoid page reload)
 }
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const startTestButton = document.getElementById("submitBtn");
-
-    startTestButton.addEventListener("click", function () {
-        // Specify the URL of the first question page
-        const questionUrl = "Questions/Q2.html";
-
-        // Open the question page in a new window or tab
-        window.open(questionUrl, "_blank");
-    });
-});
--- JS/myScript.js
+++ JS/myScript.js
@@ -1,7 +1,7 @@
 function saveResponseAndRedirect(nextQuestionURL, selectedOption) {
-            // Save the selected option to sessionStorage
-            sessionStorage.setItem('selectedOption', selectedOption);
+  // Save the selected option to sessionStorage
+  sessionStorage.setItem("selectedOption", selectedOption);
 
-            // Redirect to the next question
-            window.location.href = nextQuestionURL;
-        }
+  // Redirect to the next question
+  window.location.href = nextQuestionURL;
+}
