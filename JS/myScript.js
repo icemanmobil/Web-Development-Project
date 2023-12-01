71062395ef4c81e7f2b5597c0f487cbd6d91d2a6
icemanmobil <59970080+icemanmobil@users.noreply.github.com>
2023-11-30 23:01:25 -0500

    final changes

--- JS/myScript.js
+++ JS/myScript.js
@@ -30,7 +30,8 @@ function generateRandomPassword() {
   }
 
   // Fill in the password field with the generated password
-  document.getElementById("password").value = randomPassword;
+  const passwordElement = document.getElementById("password");
+  passwordElement.value = randomPassword;
 
   // Add the new user and password to the validCredentials array
   validCredentials.push({ username: username, password: randomPassword });
@@ -38,12 +39,14 @@ function generateRandomPassword() {
   // Display a confirmation message (you can customize this)
   alert(`New password for ${username}: ${randomPassword}`);
 
-  getElementById("password").value = randomPassword;
+  passwordElement.value = randomPassword;
 }
 
 function validateForm() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+  const usernameElement = document.getElementById("username");
+  const passwordElement = document.getElementById("password");
+  var username = usernameElement.value;
+  var password = passwordElement.value;
 
   if (username === "" || password === "") {
     alert("Please enter both username and password.");
@@ -64,9 +67,7 @@ function validateForm() {
       alert("Login successful!");
 
       // Redirect to Home.html
-      window.location.href = "HTML/Home.html";
-
-      return true; // Proceed with form submission
+      return (window.location.href = "../HTML/Home.html");
     }
   }
 
@@ -74,8 +75,8 @@ function validateForm() {
   alert("Invalid username or password. Please try again.");
 
   // Clear the username and password fields
-  document.getElementById("username").value = "";
-  document.getElementById("password").value = "";
+  usernameElement.value = "";
+  passwordElement.value = "";
 
   return false;
 }
@@ -245,8 +246,9 @@ function updateResults() {
     return optionSums[a] > optionSums[b] ? a : b;
   });
   var sameSumColors = Object.keys(optionSums).filter(function (color) {
-    return optionSums[color] === optionSums[maxOption] && color !== maxOption;
+    return optionSums[color] === optionSums[maxOption];
   });
+  console.log({ maxOption, sameSumColors });
 
   //Set background color based on the color with the most points
   if (sameSumColors.length > 0) {
@@ -267,33 +269,19 @@ function getColorForOption(option) {
 }
 
 function updateAdditionalInfo(colors) {
-  var additionalInfoElement = document.getElementById("additionalInfo");
-  var infoContent = "";
+  let additionalInfoElement = document.getElementById("additionalInfo");
+  let infoContent = "";
+  const texts = {
+    a: "<h1>Are you…Orange?</h1><br><h3>Witty…Charming…Spontaneous?<br>Impulsive…Generous…Impactful?<br>Optimistic…Eager…Bold?<br>Physical…Immediate…Courageous?<br>At school…<br>I learn by doing and experiencing, rather than by listening and reading.<br>I like being physically involved in the learning process and am motivated by my own natural competitive self and sense of fun.<br>I am a natural performer.<br>I like doing tasks that allow me to be independent and free.",
+    b: "<h1>Are you…Green?</h1><br><h3>Analytical…Global…Conceptual?<br>Cool…Calm…Collected?<br>Inventive…Logical…Problem Solver?<br>Abstract…Creative…Investigative?<br>At school…<br>I am conceptual and am an independent thinker.<br>For me, work is play.<br>I am drawn to constant challenge.<br>I like to develop models and explore ideas.",
+    c: "<h1>Are you…Blue?</h1><br><h3>Enthusiastic…Sympathetic…Personal?<br>Warm…Communicative…Compassionate?<br>Idealistic…Spiritual…Sincere?<br>Peaceful…Flexible…Imaginative?<br>At school…<br>I have a strong desire to be a role model for my classmates.<br>I am skilled at motivating and interacting with others – I make friends easily and like having friends.<br>I respond well to encouragement rather than competition.<br>I like being artistic, communicating with people, and helping people.",
+    d: "<h1>Are you…Gold?</h1><br><h3>Loyal…Dependable…Prepared?<br>Thorough…Sensible…Punctual?<br>Faithful…Stable…Organized?<br>Caring…Concerned…Helper?<br>At school…<br>I am stable and organized.<br>I am detailed-oriented and predictable.<br>I believe that work comes before play, even if I must work overtime to complete the job.<br>I understand and respect authority and am comfortable with how school goes.",
+  };
 
-  // Add information based on the color
-  if (colors.length === 1) {
-    switch (colors[0]) {
-      case "a":
-        infoContent =
-          "<h1>Are you…Orange?</h1><br><h3>Witty…Charming…Spontaneous?<br>Impulsive…Generous…Impactful?<br>Optimistic…Eager…Bold?<br>Physical…Immediate…Courageous?<br>At school…<br>I learn by doing and experiencing, rather than by listening and reading.<br>I like being physically involved in the learning process and am motivated by my own natural competitive self and sense of fun.<br>I am a natural performer.<br>I like doing tasks that allow me to be independent and free.";
-        break;
-      case "b":
-        infoContent =
-          "<h1>Are you…Green?</h1><br><h3>Analytical…Global…Conceptual?<br>Cool…Calm…Collected?<br>Inventive…Logical…Problem Solver?<br>Abstract…Creative…Investigative?<br>At school…<br>I am conceptual and am an independent thinker.<br>For me, work is play.<br>I am drawn to constant challenge.<br>I like to develop models and explore ideas.";
-        break;
-      case "c":
-        infoContent =
-          "<h1>Are you…Blue?</h1><br><h3>Enthusiastic…Sympathetic…Personal?<br>Warm…Communicative…Compassionate?<br>Idealistic…Spiritual…Sincere?<br>Peaceful…Flexible…Imaginative?<br>At school…<br>I have a strong desire to be a role model for my classmates.<br>I am skilled at motivating and interacting with others – I make friends easily and like having friends.<br>I respond well to encouragement rather than competition.<br>I like being artistic, communicating with people, and helping people.";
-        break;
-      case "d":
-        infoContent =
-          "<h1>Are you…Gold?</h1><br><h3>Loyal…Dependable…Prepared?<br>Thorough…Sensible…Punctual?<br>Faithful…Stable…Organized?<br>Caring…Concerned…Helper?<br>At school…<br>I am stable and organized.<br>I am detailed-oriented and predictable.<br>I believe that work comes before play, even if I must work overtime to complete the job.<br>I understand and respect authority and am comfortable with how school goes.";
-        break;
-    }
-  } else if (colors.length > 1) {
-    // Display a generic message for a mix of two colors
-    infoContent = "<h1>You are a Mix of Several Colors!</h1><br>";
-  }
+  colors.forEach((color) => {
+    infoContent += texts[color];
+  });
+
   // Update the content
   additionalInfoElement.innerHTML = infoContent;
 }
@@ -301,9 +289,13 @@ function updateAdditionalInfo(colors) {
 // // Helper function to set gradient background for multiple colors
 function setGradientBackground(colors) {
   var gradientColors = colors.map((color) => getColorForOption(color));
-  document.body.style.backgroundImage = `linear-gradient(to right, ${gradientColors.join(
-    ", "
-  )})`;
+  if (gradientColors.length === 1) {
+    document.body.style.background = gradientColors[0];
+  } else {
+    document.body.style.backgroundImage = `linear-gradient(to right, ${gradientColors
+      .join(", ")
+      .toLowerCase()})`;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
